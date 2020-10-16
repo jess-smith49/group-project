dd5a0c6e4165c7ff1929270adce0aa7617c3c0e3
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-16 00:28:06 -0500

    added function to retrieve input from modal

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,7 +1,19 @@
 
 // create a variable 'q' for query search and 'i' for ingredients
 
-// remove endpoints and modify fetch request to concate strings based on modal input
+// create event when search button is clicked
+$("#find-recipe").click(function(event) {
+    event.preventDefault();
+    // retrieve input from textarea
+    var textAreaEl = $(".query-search").val().trim();
+    console.log(textAreaEl);
+    console.log("button was clicked");
+
+    // concat input into fetch request to return data
+
+})
+
+// remove endpoints and modify fetch request to concat strings based on modal input
 fetch("https://rapidapi.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet", {
     "method": "GET",
     "headers": {
