b834b5c182827012f34f55a9aaf055df9a4d6a18
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-17 17:24:19 -0500

    added for loop for search results

--- assets/drink.js
+++ assets/drink.js
@@ -1,3 +1,4 @@
+
 // event function when search button is clicked
 $("#find-drink").click(function (event) {
     event.preventDefault();
@@ -12,14 +13,20 @@ $("#find-drink").click(function (event) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data);
+            // for (var i = 0; i < 11; i ++) {
+            //     let name = data.drinks[i].strDrink;
+            //     let img = data.drinks[i].strDrinkThumb;
+            //     console.log(name, img);
+        //}
             displayDrinkList(data);
-        });
+});
 
+// if searching by name, retrieve text from input field
+var drinkNameSearchEl = $("#drinkName-search").val().trim();
+console.log(drinkNameSearchEl);
 
-    // if searching by name, retrieve text from input field
-    var drinkNameSearchEl = $("#drinkName-search").val().trim();
-    console.log(drinkNameSearchEl);
-
+if (drinkNameSearchEl) {
     // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
         .then(function (response) {
@@ -30,18 +37,25 @@ $("#find-drink").click(function (event) {
             displayDrinkList(data);
 
         })
+} else {
 
-    console.log("button was clicked");
+    console.log("invalid");
+}
+
 });
 
 // function for drink search
 function displayDrinkList(cocktail) {
     console.log(cocktail.drinks[0]);
-    let drinkSection = $("#drink-results");
-    let drinkName = $("<li>").text(cocktail.drinks[0].strDrink);
-    let drinkImg = $("<img>").attr('src', cocktail.drinks[0].strDrinkThumb);
-    drinkSection.append(drinkName, drinkImg);
-
+    for (var i = 0; i < 11; i++) {
+        let name = cocktail.drinks[i].strDrink;
+        let img = cocktail.drinks[i].strDrinkThumb;
+        console.log(name, img);
+        let drinkSection = $("#drink-results");
+        let drinkName = $("<li>").text(cocktail.drinks[i].strDrink);
+        let drinkImg = $("<img>").attr('src', cocktail.drinks[i].strDrinkThumb);
+        drinkSection.append(drinkName, drinkImg);
+    }
 }
 
 
