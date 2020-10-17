2a8920fa9b0a0de0938858d548a73cc8b7df8ce9
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-17 16:45:03 -0500

    created search function

--- assets/drink.js
+++ assets/drink.js
@@ -1,18 +1,53 @@
-// function when search button is clicked
-$("#find-drink").click(function(event) {
+// event function when search button is clicked
+$("#find-drink").click(function (event) {
     event.preventDefault();
-    // retrieve input from textarea
-    var textAreaEl = $(".drink-search").val().trim();
-    console.log(textAreaEl);
+
+
+    // if searching by liquor type, retrieve text from input field
+    var liquorSearchEl = $("#liquor-search").val().trim();
+
+    // fetch data by type of liquor
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquorSearchEl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayDrinkList(data);
+        });
+
+
+    // if searching by name, retrieve text from input field
+    var drinkNameSearchEl = $("#drinkName-search").val().trim();
+    console.log(drinkNameSearchEl);
+
+    // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            displayDrinkList(data);
+
+        })
+
     console.log("button was clicked");
-})
+});
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+// function for drink search
+function displayDrinkList(cocktail) {
+    console.log(cocktail.drinks[0]);
+    let drinkSection = $("#drink-results");
+    let drinkName = $("<li>").text(cocktail.drinks[0].strDrink);
+    let drinkImg = $("<img>").attr('src', cocktail.drinks[0].strDrinkThumb);
+    drinkSection.append(drinkName, drinkImg);
 
-    
+}
+
+
+
+
+// add an 'add' button to each result, once add button is clicked, attach it to the "Your Meals" section
+
+
+
