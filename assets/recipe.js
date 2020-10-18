83b3c84d98227f5d64d69754c3c212d5100e2adf
sas1300 <sascode13@gmail.com>
2020-10-18 15:43:18 -0500

    commit before changing branches

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,40 +1,40 @@
 var mealInput = "spaghetti"
 
-fetch("https://recipe-puppy.p.rapidapi.com/?p=1&q="+ "spaghetti", {
+fetch("https://recipe-puppy.p.rapidapi.com/?p=1&q=" + "spaghetti", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
 		"x-rapidapi-key": "736dc35b66msh2bcbc7d41dedb3ap1dfeffjsn376e4d505821"
 	}
 })
-.then(function(response) {
+	.then(function (response) {
 
-  return response.json();
-  
-})
+		return response.json();
 
-.then(function(data) { 
-	console.log(data);
-	console.log(data.results.title);
+	})
 
-	
-		
-	var recipeNameEl = document.querySelector("#recipeName")
-	recipeNameEl.textContent = data.results[0].title	
-})
-	
-		
-		
+	.then(function (data) {
+		console.log(data);
+		console.log(data.results.title);
+
+
+
+		var recipeNameEl = document.querySelector("#recipeName")
+		recipeNameEl.textContent = data.results[0].title
+	})
+
+
+
 
 // 	var recipeName = document.createElement("title");
-	
+
 // 	document.innerHTML= results.title;
 
 // 	console.log(data.results[0].title) 
 
 // var recipeName.textContent = data.results[0].title	
 
-	
+
 
 
 //});
