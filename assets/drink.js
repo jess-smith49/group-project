a9c27306525c11ba1ce05e9f47161e18439485c1
sas1300 <sascode13@gmail.com>
2020-10-18 17:57:58 -0500

    final-commit-recpup-change APIs

--- assets/drink.js
+++ assets/drink.js
@@ -2,3 +2,4 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then(response => response.json())
     .then(data => console.log(data));
     
+ 
---
+++ assets/recipe.js
@@ -0,0 +1,52 @@
+var mealInput = "spaghetti"
+
+fetch("https://recipe-puppy.p.rapidapi.com/?p=1&q="+ "spaghetti", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+		"x-rapidapi-key": "736dc35b66msh2bcbc7d41dedb3ap1dfeffjsn376e4d505821"
+	}
+})
+.then(function(response) {
+
+  return response.json(); 
+})
+
+.then(function(data) { 
+	console.log(data)
+
+	var recipeNameEl = document.querySelector("#recipe-results-container")
+    recipeNameEl.textContent = data.results[0].title		
+})
+	
+		
+		
+
+// 	var recipeName = document.createElement("title");
+	
+// 	document.innerHTML= results.title;
+
+// 	console.log(data.results[0].title) 
+
+// var recipeName.textContent = data.results[0].title	
+
+	
+
+
+//});
+
+//recipeName.setAttribute(thumbnail);  for image.  Add later
+
+// var recipeName = document.createElement("title");
+
+// 	document.innerHTML= results.title;
+
+// // 	var responseContainerEl = document.querySelector("#response-container");
+// 	console.log(data.results[0].title) 
+
+// // 	var recipeName = document.createElement("title");
+// var recipeName.textContent = data.results[0].title
+
+// //recipeName.setAttribute(response.data.title);
+
+// // 	responseContainerEl.appendChild(gifImg);
