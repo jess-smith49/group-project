c7eaecc67efaba647cde3bc69d8eaddbbdb4e649
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-19 17:54:34 -0500

    fixed function to include ingredients and append to modal

--- assets/drink.js
+++ assets/drink.js
@@ -12,51 +12,68 @@ $("#find-drink").click(function (event) {
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
-            // for (var i = 0; i < 11; i ++) {
-            //     let name = data.drinks[i].strDrink;
-            //     let img = data.drinks[i].strDrinkThumb;
-            //     console.log(name, img);
-        //}
-            displayDrinkList(data);
-});
-
-// if searching by name, retrieve text from input field
-var drinkNameSearchEl = $("#drinkName-search").val().trim();
-console.log(drinkNameSearchEl);
-
-if (drinkNameSearchEl) {
-    // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
+        // retrieve cocktail ID and run search by ID
         .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            // console.log(data);
-            displayDrinkList(data);
+            for (var i = 0; i < 11; i++) {
+                let drinkId = response.drinks[i].idDrink;
+                //console.log(drinkId)
+                fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+                        displayDrinkList(data);
+                    })
+            }
+        });
 
-        })
-} else {
 
-    console.log("invalid");
-}
+    // if searching by name, retrieve text from input field
+    var drinkNameSearchEl = $("#drinkName-search").val().trim();
+    console.log(drinkNameSearchEl);
+
+    if (drinkNameSearchEl) {
+        // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                displayDrinkList(data);
+            })
+            // clear out user input
+            .then(function () {
+                return $("#drinkName-search").empty();
+            })
+    } else {
+        // display a message to user that there is no result and run a new search
+        console.log("no results")
+    }
 
 });
 
-// function for drink search
+// function to display drinks in modal
 function displayDrinkList(cocktail) {
-    console.log(cocktail.drinks[0]);
     for (var i = 0; i < 11; i++) {
-        let name = cocktail.drinks[i].strDrink;
-        let img = cocktail.drinks[i].strDrinkThumb;
-        console.log(name, img);
         let drinkSection = $("#drink-results");
         let drinkName = $("<li>").text(cocktail.drinks[i].strDrink);
         let drinkImg = $("<img>").attr('src', cocktail.drinks[i].strDrinkThumb);
+        let drinkIns = $("<p>").text(cocktail.drinks[i].strInstructions);
         drinkSection.append(drinkName, drinkImg);
+        for (var k = 1; k < 16; k++) {
+            if (cocktail.drinks[i][`strIngredient${k}`] == null || cocktail.drinks[i][`strIngredient${k}`] == "") {
+                break;
+            } else {
+
+                drinkData = $("<li>").text(cocktail.drinks[i][`strIngredient${k}`])
+    
+                drinkSection.append(drinkData, drinkIns);
+            }
+        }
+        
     }
-}
+};
 
 
 
