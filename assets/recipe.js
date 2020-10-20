9949686994963616919ad9b17ae994f3e8883841
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-19 20:10:05 -0500

    updated recipe js to Sheila's current code

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,33 +1,70 @@
-// Modal Functions 
+console.dir(window.document);
 
-// create a variable 'q' for query search and 'i' for ingredients
+//Request and Response-Functioning (example search pasta & max fat)
 
-// create event when search button is clicked
-$("#find-recipe").click(function(event) {
-    event.preventDefault();
-    // retrieve input from textarea
-    var textAreaEl = $(".query-search").val().trim();
-    console.log(textAreaEl);
-    console.log("button was clicked");
+var recipeNameEl = "recipe-results"
 
-    // concat input into fetch request to return data
+fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=5e85b9e168244c1cbbe7ed190aca128b")
 
-})
+.then(response => response.json())
 
-// remove endpoints and modify fetch request to concat strings based on modal input
-fetch("https://rapidapi.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
-        "x-rapidapi-key": "358cb2fa9amshbe93bf171c704afp1d857cjsn9e3fac832eff"
-    }
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+.then(data => console.log(data)); {
+
+for (var i = 0; i < 11; i ++) {
+	let title = data.results.title[i];
+	let image = data.results.image[i];
+	console.log(title, image);
+}
+	displayRecipeList(data);
+
+}
+
+
+
+// DONE - Used bootstrap in HTML.   Click event for when "Click here to begin" button is selected-
+////DONE - Used bootstrap in HTML.  Upon click, modal will open with two text entry boxes and a search button
+	// Modal opens with heading "Search Here for a Recipe"
+
+//Search by ingredient Search Box
+	// clear search box after click event
+	//highlight box if search data entered
+	//account for invalid data entry
+
+//Search by cusine type Search Box
+	// clear search box after click event
+	//highlight box if search data entered
+	//account for invalid data entry
+
+//Search button - Click event
+//Do we need a second search button.  If yes, will cut down on a lot of if/then.  Currently, if we use one search button and user enters text into both boxes it will search for both.  We could also throw an error - enter information in one box only.
+
+//Return 10 items.  Name of recipe, thumbnail and ingredient list/instructions
+  //Second Modal????  New window????
+
+
+
+
+  //event.preventDefault();
+
+	// .then(function(data)
+	// 	console.log(data)
+	
+	// })
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelector("#recipe-results").innerHTML = "Recipe Name: " + data.results[0].title
+
+
+// &query=pasta&maxFat=25&
 
 
 
