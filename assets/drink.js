c27da81d2394283bf10df032102781134ffa0074
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-19 19:51:34 -0500

    updated drink js to clear user input and remove any null ingredients/measurements

--- assets/drink.js
+++ assets/drink.js
@@ -25,48 +25,61 @@ $("#find-drink").click(function (event) {
                         console.log(data);
                         displayDrinkList(data);
                     })
-            }
-        });
-
-
-    // if searching by name, retrieve text from input field
-    var drinkNameSearchEl = $("#drinkName-search").val().trim();
-    console.log(drinkNameSearchEl);
-
-    if (drinkNameSearchEl) {
-        // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                displayDrinkList(data);
+                }
             });
-
-        // reset input field
-
-    } else {
-        // display a message to user that there is no result and run a new search
-        console.log("no results")
-    }
-
-});
-
+            
+            
+            // if searching by name, retrieve text from input field
+            var drinkNameSearchEl = $("#drinkName-search").val().trim();
+            console.log(drinkNameSearchEl);
+            
+            if (drinkNameSearchEl) {
+                // fetch data by drink name (note-if the drink name is two words long, the two words should be separated by an underscore)
+                fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameSearchEl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    displayDrinkList(data);
+                });
+                
+                // reset input field
+                
+            } else {
+                // display a message to user that there is no result and run a new search
+                console.log("no results")
+            }
+            
+            // clear out the text input field
+            $("#liquor-search").val("");
+            $("#drinkName-search").val("");
+            
+        });
+        
 // function to display drinks in modal
 function displayDrinkList(cocktail) {
+    // create a loop to go through the array and return up to 10 drinks
     for (var i = 0; i < 11; i++) {
         let drinkSection = $("#drink-results");
         let drinkName = $("<li>").text(cocktail.drinks[i].strDrink);
         let drinkImg = $("<img>").attr('src', cocktail.drinks[i].strDrinkThumb);
         let drinkIns = $("<p>").text(cocktail.drinks[i].strInstructions);
         drinkSection.append(drinkName, drinkImg);
+        // create a loop to go through and return the list of ingredients
         for (var k = 1; k < 16; k++) {
-            if (cocktail.drinks[i][`strIngredient${k}`] == null || cocktail.drinks[i][`strIngredient${k}`] == "") {
+            // check if any of the ingredients are null or empty
+            if (cocktail.drinks[i][`strIngredient${k}`] === null || cocktail.drinks[i][`strIngredient${k}`] === "") {
                 break;
             } else {
+                // check if any of the measurements are null or empty
+                if (cocktail.drinks[i][`strMeasure${k}`] === null) {
+                    drinkData = $("<li>").text(cocktail.drinks[i][`strIngredient${k}`])
 
-                drinkData = $("<li>").text(cocktail.drinks[i][`strMeasure${k}`] + ' : ' + cocktail.drinks[i][`strIngredient${k}`])
-
+                } else {
+                    // retrieve the measurement and ingredients
+                    drinkData = $("<li>").text(cocktail.drinks[i][`strMeasure${k}`] + ' : ' + cocktail.drinks[i][`strIngredient${k}`])
+                }
+                // append to modal
                 drinkSection.append(drinkData, drinkIns);
             }
         }
@@ -74,10 +87,6 @@ function displayDrinkList(cocktail) {
     }
 };
 
-
-
-
-// add an 'add' button to each result, once add button is clicked, attach it to the "Your Meals" section
 
 
 
