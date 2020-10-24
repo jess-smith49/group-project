85c39ea2b855daa8f59f9887cec5774797c2e282
jess-smith49 <js.jessicasmith774@gmail.com>
2020-10-24 14:50:46 -0500

    random recipe functionality

--- assets/recipe.js
+++ assets/recipe.js
@@ -20,7 +20,9 @@ $("#find-recipe").click(function(event){
 		
 		//getting recipe id and searching by the id
          for(var k = 0; k < 3; k++){
-             let recipeId = response.meals[k].idMeal;
+            var randNum = Math.floor(response.meals.length * Math.random());
+            let recipeId = response.meals[randNum].idMeal;
+            console.log(recipeId);
 
             fetch(
             `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`
